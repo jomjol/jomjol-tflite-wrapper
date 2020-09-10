afa0ec5ad8ab7a85bfd6f173fec1f42ee0c610f1
jomjol <30766535+jomjol@users.noreply.github.com>
2020-09-10 16:44:54 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # jomjol-tflite-wrapper
-Example of tflite c++ class wrapper
+Here the essentially part of a c++ wrapper class for the Tensorfloor Tflite for an ESP32 is demonstrated. The basic idea or task, which is triggering my activity is coming from an AI-on-the-edge device, also published in Github: https://github.com/jomjol/AI-on-the-edge-device/wiki
+
+But as the essential part on the Tflite integration is deeply covered inside the surrounding code, I have decided to polish one of my testing routines as an example on how to use the wrapper.
+
+I started from the c++ tflite library inside Tensorflow and wanted an easy customized implementation, with the following properties:
+* easy to handle on a ESP32 system
+* capability to run **models bigger than 1 MByte**
+* ideally wrapped into a c++ class to be better capsuled for my project
+* **direct loading of tflite-files from a SD-card**, to separate the code from the content/task and much leaner deployment of modifications (code and CNN wise)
+
+Therefore the basic extract of this idea can be found in the attached library. It consists of two parts:
+
+1.  Tensorflow Tflite library [/code/lib/tflite] - with modification to increase the possible model size
+2. CTfLiteClass as the c++ wrapper handling tflite loading, input loading and readout. This can be found here: [/code/lib/jomjol_tfliteclass]
+
+The basic usage for a classification network is as easy as follows:
+
+        classtflite = new CTfLiteClass; 
+        classtflite->LoadModel("cnn_file.tflite"); 
+        classtflite->MakeAllocate();  
+        classtflite->LoadInputImage("input.bmp");
+        classtflite->Invoke();
+        result = classtflite->GetOutClassification();
+        delete classtflite;
+
+
+
+The surrounding code is implemented in the ESP32 Espressif SDK using the PlatformIO development environment.
+
+To run this you need a ESP32 with SD-card and PSRAM. The  additional PSRAM is needed to run the big models I use (even in tflite coding > 1MByte). I tested this code on a ESP32CAM (the same than in the main project. On the SD-card you need to copy the content of [/sd-card] to the root directory.
+
+
+
+In the main routine you find two testing loops for two types of models with iterating tflite files, which are loaded one after the other and tested on 5 test images each. The time needed is written to the serial interface.
+
+
+
+The following CNN-files are tested. The details to the CNN and the task can be found following the above link. This repository is only to extract the very essential part of the tflite and not the neural network background.
+
+
+
+##### 1. CNN digit
+
+This is a classification network, that is used to recognize digits out of an image and transfer into the digits 0 ... 9, including a 10th class for NaN (Not a Number).
+##### 2. CNN analog
+This is also a CNN network with two output neurons, which encode as the value of a sinus and cosinus number the angle of a pointer of the input image.
+
---
+++ code/src/main.cpp
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include "driver/gpio.h"
+
+#include <string>
+#include <math.h>
+
+#include "CTfLiteClass.h"
+
+static const char *TAGMAIN = "main";
+#define FLASH_GPIO GPIO_NUM_4
+
+void Init_SDCard_GPIO()
+{
+    ESP_LOGI(TAGMAIN, "Initializing SD card");
+    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+    host.flags = SDMMC_HOST_FLAG_1BIT;
+    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = { };
+    mount_config.format_if_mount_failed = false;
+    mount_config.max_files = 5;
+
+    sdmmc_card_t* card;
+    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAGMAIN, "Failed to mount filesystem.");
+        } else {
+            ESP_LOGE(TAGMAIN, "Failed to initialize the card (%d). Make sure SD card lines have pull-up resistors in place.", ret);
+        }
+        return;
+    }
+    sdmmc_card_print_info(stdout, card);
+
+	// Init the GPIO and turn of flash light (sometime switched on after startup)
+    gpio_pad_select_gpio(FLASH_GPIO);
+    gpio_set_direction(FLASH_GPIO, GPIO_MODE_OUTPUT);  
+    gpio_set_level(FLASH_GPIO, 0);   
+}
+
+
+void test_digit_model()
+    {
+    CTfLiteClass *classtflite;
+
+    float result;
+    uint32_t starttime, stoptime, loadtime, calctime, loadtime_sum, calctime_sum;
+    std::string name_tflite, name_image;
+    int tflite, image;
+    int num_tflite = 4;
+
+    for (tflite = 0; tflite < num_tflite; ++tflite)
+    {
+        name_tflite = "/sdcard/digit" + std::to_string(tflite) + ".tflite";
+        printf("\n====== Model digit%d.tflite ====================\n  ", tflite);
+        starttime = esp_log_timestamp();
+        classtflite = new CTfLiteClass; 
+        classtflite->LoadModel(name_tflite); 
+        classtflite->MakeAllocate();  
+        stoptime = esp_log_timestamp();
+        printf("Model loading time: %dms\n  ", stoptime-starttime);
+        loadtime_sum = 0; 
+        calctime_sum = 0;
+        starttime = stoptime;
+
+        for (image = 0; image < 5; ++image)
+        {
+            name_image = "/sdcard/digit" + std::to_string(image) + ".bmp";
+
+            classtflite->LoadInputImage(name_image);
+
+            stoptime = esp_log_timestamp(); 
+            loadtime = stoptime - starttime; starttime = stoptime;
+
+            classtflite->Invoke();
+
+            stoptime = esp_log_timestamp(); 
+            calctime = stoptime - starttime; starttime = stoptime;
+
+            result = classtflite->GetOutClassification();
+
+            loadtime_sum += loadtime;
+            calctime_sum += calctime;
+            printf("Image: %s - CNN-Classification: %d - Loadtime: %dms, Calctime: %dms\n  ", name_image.c_str(), (int) result, loadtime, calctime);
+        }
+
+        printf("Average Loadtime: %dms, Calctime: %dms\n", (int) (loadtime_sum/5),  (int) (calctime_sum/5)); 
+
+        delete classtflite;
+    }
+}
+
+void test_analog_model()
+    {
+    CTfLiteClass *classtflite;
+
+    float f1, f2, result;
+    uint32_t starttime, stoptime, loadtime, calctime, loadtime_sum, calctime_sum;
+    std::string name_tflite, name_image;
+    int tflite, image;
+    int num_tflite = 5;
+
+    for (tflite = 0; tflite < num_tflite; ++tflite)
+    {
+        name_tflite = "/sdcard/analog" + std::to_string(tflite) + ".tflite";
+        printf("\n====== Model analog%d.tflite ====================\n  ", tflite);
+        starttime = esp_log_timestamp();
+        classtflite = new CTfLiteClass; 
+        classtflite->LoadModel(name_tflite); 
+        classtflite->MakeAllocate();  
+        stoptime = esp_log_timestamp();
+        printf("Model loading time: %dms\n  ", stoptime-starttime);
+        loadtime_sum = 0; 
+        calctime_sum = 0;
+        starttime = stoptime;
+
+        for (image = 0; image < 5; ++image)
+        {
+            name_image = "/sdcard/pointer" + std::to_string(image) + ".bmp";
+            classtflite->LoadInputImage(name_image);
+
+            stoptime = esp_log_timestamp(); 
+            loadtime = stoptime - starttime; starttime = stoptime;
+
+            classtflite->Invoke();
+
+            stoptime = esp_log_timestamp(); 
+            calctime = stoptime - starttime; starttime = stoptime;
+
+            f1 = classtflite->GetOutputValue(0);
+            f2 = classtflite->GetOutputValue(1);
+            result = fmod(atan2(f1, f2) / (M_PI * 2) + 2, 1) * 10;
+            loadtime_sum += loadtime;
+            calctime_sum += calctime;
+            printf("Image: %s - CNN-Result: %f - Loadtime: %dms, Calctime: %dms\n  ", name_image.c_str(), result, loadtime, calctime);
+        }
+
+        printf("Average Loadtime: %dms, Calctime: %dms\n", (int) (loadtime_sum/5),  (int) (calctime_sum/5));
+
+        delete classtflite;
+
+    }
+}
+
+void doTask(void *pvParameter)
+    {
+        printf("\n\n========= ANALOG MODELS ==================================================\n");
+        test_analog_model();
+        printf("========= CLASSIFICATION MODELS ==========================================\n");
+        test_digit_model();
+
+        printf("\n\n\nTest is finished --> going to infite loop until hard reboot.\n");
+        
+        while (1) {
+            vTaskDelay(10);
+        };
+    }
+
+extern "C" void app_main()
+{
+    esp_log_level_set("*", ESP_LOG_ERROR);
+    Init_SDCard_GPIO();
+
+    xTaskCreate(&doTask, "doTask", configMINIMAL_STACK_SIZE * 64, NULL, 5, NULL);
+}
